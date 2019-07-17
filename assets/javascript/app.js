f45059e6f010e930cc480def23b3f2bb83f30dd6
Adrian Perez <adrian.perez87@gmail.com>
2019-07-16 21:37:50 -0700

    added styling and pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+//create object with question response options
+
+//toggle true or false
+
+// limit selection to only 1
+
+// set a timer for question to be answered
+
+// when timer runs out, end game and reset
+
+// show number of questions answered correctly or incorrectly
+
+
+
